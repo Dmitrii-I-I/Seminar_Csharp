4f58ba034d9b6c723ff6ead757b7124aa6d2290d
Dmitrii <idi-1975@yandex.ru>
2022-11-26 01:29:53 +0300

    ДЗ 5 задача 34,36,37,38

--- DZ5/Program.cs
+++ DZ5/Program.cs
@@ -23,13 +23,13 @@ return count;
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19         [-4, -6, 89, 6] -> 0
-Console.Write("Введите число элементов массива: ");
+/* Console.Write("Введите число элементов массива: ");
 int size = int.Parse(Console.ReadLine()!);
 Console.Write("Введите нижнюю границу массива: ");
 int minValue = int.Parse(Console.ReadLine()!);
 Console.Write("Введите вернюю границу массива: ");
 int maxValue = int.Parse(Console.ReadLine()!);
-if (size>0 && minValue < maxValue){
+if (size>0 && (minValue < maxValue)){
 int[] array = GetArray(size, minValue, maxValue);
 Console.WriteLine($"[ {String.Join(", ", array)} ]");
 Console.WriteLine($"Сумма элнментов с нечетными индексами: {SumElem(array)}");
@@ -49,10 +49,65 @@ int SumElem(int[] array){
     if (i%2 != 0) summa += array[i]; 
    }
     return summa;
-}
+} */
 
 // Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
 // [1 2 3 4 5] -> 5 8 3         [6 7 3 6] -> 36 21
+/* Console.Write("Введите число элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+Console.Write("Введите нижнюю границу массива: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите вернюю границу массива: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+if (size>0 && (minValue < maxValue)){
+int[] array = GetArray(size, minValue, maxValue);
+Console.WriteLine($"[ {String.Join(", ", array)} ]");
+Console.WriteLine($"Произведения пар чисел: [ {String.Join(", ", number_multip(array))} ]");
+}
+else Console.WriteLine("Не коректные входные данные");
+
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] result = new int[size];
+    for (int i=0; i<size; i++){
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return result;
+}
+int[] number_multip(int[] array){
+   int[] array_mulpip = new int[array.Length/2];
+   for (int i=0; i<array_mulpip.Length; i++)   {
+    array_mulpip[i] = array[i]*array[array.Length-i-1]; 
+   }
+    return array_mulpip;
+} */
 
 // Задача 38: Задайте массив целых чисел от -10 до 10. Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+Console.Write("Введите число элементов массива: ");
+int size = int.Parse(Console.ReadLine()!);
+Console.Write("Введите нижнюю границу массива: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите вернюю границу массива: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+if (size>0 && (minValue < maxValue)){
+int[] array = GetArray(size, minValue, maxValue);
+Console.WriteLine($"[ {String.Join(", ", array)} ]");
+Console.WriteLine($"Разница между максумумом и минимумом: {DiffMember(array)}");
+}
+else Console.WriteLine("Не коректные входные данные");
+
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] result = new int[size];
+    for (int i=0; i<size; i++){
+        result[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return result;
+}
+int DiffMember(int[] array){
+   int min=array[0], max=array[0];
+   for (int i=0; i<array.Length; i++){
+     if (array[i] < min) min = array[i];
+     if (array[i] > max) max = array[i];  
+   }
+    return (max-min);
+}
